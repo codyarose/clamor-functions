5c003a5277950047d2163f95199f2fb5ebda133a
codyarose <crose992@gmail.com>
2020-08-12 14:21:57 -0500

    Set getIdToken to true to force refresh token

--- functions/handlers/users.ts
+++ functions/handlers/users.ts
@@ -1,10 +1,14 @@
-import { db, admin } from '../util/admin'
-import { Request, Response } from 'express'
-import firebase from 'firebase'
+import { db, admin } from "../util/admin"
+import { Request, Response } from "express"
+import firebase from "firebase"
 
-import config from '../util/config'
-import { User, Posts, Credentials, Notification, Likes } from './types'
-import { validateSignupData, validateLoginData, reduceUserDetails } from '../util/validators'
+import config from "../util/config"
+import { User, Posts, Credentials, Notification, Likes } from "./types"
+import {
+	validateSignupData,
+	validateLoginData,
+	reduceUserDetails,
+} from "../util/validators"
 
 firebase.initializeApp(config)
 
@@ -17,20 +21,22 @@ export const signup = async (req: Request, res: Response): Promise<unknown> => {
 
 	if (!valid) return res.status(400).json(errors)
 
-	const noImg = 'no-image.png'
+	const noImg = "no-image.png"
 
 	try {
 		const user = await db.doc(`/users/${newUser.handle}`).get()
 
 		if (user.exists) {
-			return res.status(400).json({ handle: 'This username is already taken.' })
+			return res
+				.status(400)
+				.json({ handle: "This username is already taken." })
 		}
 
 		const createdUser: firebase.auth.UserCredential = await firebase
 			.auth()
 			.createUserWithEmailAndPassword(newUser.email, newUser.password)
 
-		const token = await createdUser.user?.getIdToken()
+		const token = await createdUser.user?.getIdToken(true)
 		const userCredentials = {
 			handle: newUser.handle,
 			email: newUser.email,
@@ -43,10 +49,12 @@ export const signup = async (req: Request, res: Response): Promise<unknown> => {
 		return res.status(201).json({ token })
 	} catch (err) {
 		console.error(err)
-		if (err.code === 'auth/email-already-in-use') {
-			return res.status(400).json({ email: 'Email is already in use' })
+		if (err.code === "auth/email-already-in-use") {
+			return res.status(400).json({ email: "Email is already in use" })
 		} else {
-			return res.status(500).json({ general: 'Something went wrong, please try again.' })
+			return res
+				.status(500)
+				.json({ general: "Something went wrong, please try again." })
 		}
 	}
 }
@@ -61,30 +69,40 @@ export const login = async (req: Request, res: Response): Promise<unknown> => {
 	if (!valid) return res.status(400).json(errors)
 
 	try {
-		const signIn = await firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+		const signIn = await firebase
+			.auth()
+			.signInWithEmailAndPassword(user.email, user.password)
 
-		const token = await signIn.user?.getIdToken()
+		const token = await signIn.user?.getIdToken(true)
 
 		return res.json({ token })
 	} catch (err) {
 		console.error(err)
-		return res.status(403).json({ general: 'Wrong credentials, please try again' })
+		return res
+			.status(403)
+			.json({ general: "Wrong credentials, please try again" })
 	}
 }
 
-export const addUserDetails = async (req: Request, res: Response): Promise<unknown> => {
+export const addUserDetails = async (
+	req: Request,
+	res: Response
+): Promise<unknown> => {
 	const userDetails = reduceUserDetails(req.body)
 
 	try {
 		await db.doc(`/users/${req.user?.handle}`).update(userDetails)
-		return res.json({ message: 'Details added successfully' })
+		return res.json({ message: "Details added successfully" })
 	} catch (err) {
 		console.error(err)
 		return res.status(500).json({ error: err.code })
 	}
 }
 
-export const getUserDetails = async (req: Request, res: Response): Promise<unknown> => {
+export const getUserDetails = async (
+	req: Request,
+	res: Response
+): Promise<unknown> => {
 	const userData: {
 		user: User
 		posts: Posts[]
@@ -94,13 +112,14 @@ export const getUserDetails = async (req: Request, res: Response): Promise<unkno
 	}
 	try {
 		const userDoc = await db.doc(`/users/${req.params.handle}`).get()
-		if (!userDoc.exists) return res.status(404).json({ error: 'User not found' })
+		if (!userDoc.exists)
+			return res.status(404).json({ error: "User not found" })
 
 		userData.user = userDoc.data() as User
 		const userPosts = await db
-			.collection('posts')
-			.where('userHandle', '==', req.params.handle)
-			.orderBy('createdAt', 'desc')
+			.collection("posts")
+			.where("userHandle", "==", req.params.handle)
+			.orderBy("createdAt", "desc")
 			.get()
 		userPosts.forEach((doc) => {
 			userData.posts.push({
@@ -120,7 +139,10 @@ export const getUserDetails = async (req: Request, res: Response): Promise<unkno
 	return res.json(userData)
 }
 
-export const getAuthenticatedUser = async (req: Request, res: Response): Promise<unknown> => {
+export const getAuthenticatedUser = async (
+	req: Request,
+	res: Response
+): Promise<unknown> => {
 	const userData: {
 		credentials?: Credentials
 		likes?: Likes[]
@@ -132,14 +154,17 @@ export const getAuthenticatedUser = async (req: Request, res: Response): Promise
 
 		userData.credentials = userDoc.data()
 
-		const userLikes = await db.collection('likes').where('userHandle', '==', req.user?.handle).get()
+		const userLikes = await db
+			.collection("likes")
+			.where("userHandle", "==", req.user?.handle)
+			.get()
 
 		userData.likes = []
 		userLikes.forEach((doc) => userData.likes?.push(doc.data() as Likes))
 		const userNotifications = await db
-			.collection('notifications')
-			.where('recipient', '==', req.user?.handle)
-			.orderBy('createdAt', 'desc')
+			.collection("notifications")
+			.where("recipient", "==", req.user?.handle)
+			.orderBy("createdAt", "desc")
 			.limit(10)
 			.get()
 		userData.notifications = []
@@ -152,7 +177,7 @@ export const getAuthenticatedUser = async (req: Request, res: Response): Promise
 				type: doc.data().type,
 				read: doc.data().read,
 				notifications: doc.id,
-			}),
+			})
 		)
 		return res.json(userData)
 	} catch (err) {
@@ -161,11 +186,14 @@ export const getAuthenticatedUser = async (req: Request, res: Response): Promise
 	}
 }
 
-export const uploadImage = async (req: Request, res: Response): Promise<unknown> => {
-	const { default: BusBoy } = await import('busboy')
-	const path = await import('path')
-	const os = await import('os')
-	const fs = await import('fs')
+export const uploadImage = async (
+	req: Request,
+	res: Response
+): Promise<unknown> => {
+	const { default: BusBoy } = await import("busboy")
+	const path = await import("path")
+	const os = await import("os")
+	const fs = await import("fs")
 
 	const busboy = new BusBoy({ headers: req.headers })
 
@@ -174,24 +202,36 @@ export const uploadImage = async (req: Request, res: Response): Promise<unknown>
 			filePath: string
 			mimetype?: unknown
 		} = {
-			filePath: '',
+			filePath: "",
 		}
 
 	busboy.on(
-		'file',
-		(_fieldname: string, file: NodeJS.ReadableStream, filename: string, _encoding: string, mimetype: string) => {
-			if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
-				return res.status(400).json({ error: 'Wrong file type submitted' })
+		"file",
+		(
+			_fieldname: string,
+			file: NodeJS.ReadableStream,
+			filename: string,
+			_encoding: string,
+			mimetype: string
+		) => {
+			if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+				return res
+					.status(400)
+					.json({ error: "Wrong file type submitted" })
 			}
-			const imageExtension = filename.split('.')[filename.split('.').length - 1]
-			imageFileName = `${Math.round(Math.random() * 100000000000)}.${imageExtension}`
+			const imageExtension = filename.split(".")[
+				filename.split(".").length - 1
+			]
+			imageFileName = `${Math.round(
+				Math.random() * 100000000000
+			)}.${imageExtension}`
 			const filePath = path.join(os.tmpdir(), imageFileName)
 			imageToBeUploaded = { filePath, mimetype }
 			file.pipe(fs.createWriteStream(filePath))
 			return
-		},
+		}
 	)
-	busboy.on('finish', async () => {
+	busboy.on("finish", async () => {
 		try {
 			await admin
 				.storage()
@@ -206,7 +246,7 @@ export const uploadImage = async (req: Request, res: Response): Promise<unknown>
 				})
 			const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`
 			await db.doc(`/users/${req.user?.handle}`).update({ imageUrl })
-			return res.json({ message: 'Image uploaded successfully' })
+			return res.json({ message: "Image uploaded successfully" })
 		} catch (err) {
 			return res.status(500).json({ error: err.code })
 		}
@@ -215,7 +255,10 @@ export const uploadImage = async (req: Request, res: Response): Promise<unknown>
 	return
 }
 
-export const markNotificationsRead = async (req: Request, res: Response): Promise<unknown> => {
+export const markNotificationsRead = async (
+	req: Request,
+	res: Response
+): Promise<unknown> => {
 	try {
 		const batch = db.batch()
 		req.body.forEach((notificationId: string) => {
@@ -223,7 +266,7 @@ export const markNotificationsRead = async (req: Request, res: Response): Promis
 			batch.update(notification, { read: true })
 		})
 		await batch.commit()
-		return res.json({ message: 'Notifications marked read' })
+		return res.json({ message: "Notifications marked read" })
 	} catch (err) {
 		console.error(err)
 		return res.status(500).json({ error: err.code })
